167c0b89c46d168a45548b0cff318c1b76a459dd
wmw_drewgarratt <drew.garratt@wemakewebsites.com>
2022-11-03 11:51:49 +0000

    chore: dep updates
    [ci skip]

---
+++ apps/shopify-theme/frontend/entrypoints/theme.ts
@@ -0,0 +1,14 @@
+import 'vite/modulepreload-polyfill';
+
+const element: HTMLDivElement = document.createElement('div');
+element.className = 'app';
+element.innerHTML = `
+  <h1>Hello Vite!</h1>
+  <div class="card">
+    <button id="counter" type="button"></button>
+  </div>
+  <p class="read-the-docs">
+    Click on the Vite logo to learn more
+  </p>
+`;
+document.body.appendChild(element);
--- apps/shopify-theme/vite.config.ts
+++ apps/shopify-theme/vite.config.ts
@@ -14,4 +14,7 @@ export default {
       additionalEntrypoints: [],
     }),
   ],
+  build: {
+    emptyOutDir: false,
+  },
 };
